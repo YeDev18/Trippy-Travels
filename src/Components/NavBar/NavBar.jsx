2ec90b427a8e9ae2d45429335a41347a4d4f11d6
Emmanuel252 <ouattarayoussoufemmanuel@gmail.com>
2023-06-02 22:42:58 +0000

    First Commit

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import './NavBar.css';
+import { MenuItems } from './MenuItems';
+import { useState } from 'react';
+const Navbar = () => {
+  const [clicked, setClicked] = useState(false);
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+  return (
+    <>
+      <nav className="NavbarItems">
+        <h1 className="navbar-logo">Trippy</h1>
+
+        <div className="menu-icons">
+          <i
+            className={clicked ? 'fas fa-times' : 'fas fa-bars'}
+            onClick={handleClick}
+          ></i>
+        </div>
+
+        <ul className={clicked ? 'nav-menu active' : 'nav-menu'}>
+          {' '}
+          {/* on ne peut ajouter de point aactive sinon cela deforme les emplacement
+          des items */}
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a href={item.url} className={item.cName}>
+                  <i class={item.icon}></i>
+                  {item.title}
+                </a>
+              </li>
+            );
+          })}
+          <button>Sign Up</button>
+        </ul>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
